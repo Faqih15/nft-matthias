016c692f82dc892a3d4486ec3bd9f27185b3f04a
Mujahid Faqihuddin 32015 <jahidiyam1592@gmail.com>
2022-11-15 11:15:12 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'courier-prime': ['"Courier Prime"'],
+      },
+    },
+    plugins: [],
+  }
